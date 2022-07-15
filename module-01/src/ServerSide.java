a0a3ba0dd6044f5d0a7dc81f0d95a9f57c74dcfc
oshadaera68 <mindartlk010@gmail.com>
2022-07-15 18:19:17 +0530

    updated content

--- module-01/src/ServerSide.java
+++ module-01/src/ServerSide.java
@@ -1,5 +1,10 @@
-public class ServerSide {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class ServerSide {
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8000;
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Server is up and running on port:" +PORT);
     }
 }
